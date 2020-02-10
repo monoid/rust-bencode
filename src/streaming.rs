d6564ed3c4d1b09d6bc08648eb11106b07a2df65
Ivan Boldyrev <lispnik@gmail.com>
2020-02-10 21:49:17 +0300

    Get rid of obsolete ... ranges

--- src/streaming.rs
+++ src/streaming.rs
@@ -123,10 +123,10 @@ impl<T: Iterator<Item=u8>> StreamingParser<T> {
         let mut num = 0;
         match self.curr_char() {
             Some('0') => self.next_byte(),
-            Some('1' ... '9') => {
+            Some('1' ..= '9') => {
                 loop {
                     match self.curr_char() {
-                        Some(ch @ '0' ... '9') => self.parse_digit(ch, sign, &mut num),
+                        Some(ch @ '0' ..= '9') => self.parse_digit(ch, sign, &mut num),
                         _ => break
                     }
                     self.next_byte();
@@ -141,10 +141,10 @@ impl<T: Iterator<Item=u8>> StreamingParser<T> {
     fn parse_length_digits(&mut self, sign: i64) -> Result<i64, Error> {
         let mut num = 0;
         match self.curr_char() {
-            Some('0' ... '9') => {
+            Some('0' ..= '9') => {
                 loop {
                     match self.curr_char() {
-                        Some(ch @ '0' ... '9') => self.parse_digit(ch, sign, &mut num),
+                        Some(ch @ '0' ..= '9') => self.parse_digit(ch, sign, &mut num),
                         _ => break
                     }
                     self.next_byte();
@@ -184,7 +184,7 @@ impl<T: Iterator<Item=u8>> StreamingParser<T> {
     fn parse_key(&mut self) -> Result<BencodeEvent, Error> {
         check_nesting!(self);
         match self.curr_char() {
-            Some('0' ... '9') => {
+            Some('0' ..= '9') => {
                 self.decoded += 1;
                 let res = self.parse_bytestring()?;
                 Ok(DictKey(res))
@@ -204,7 +204,7 @@ impl<T: Iterator<Item=u8>> StreamingParser<T> {
                 let res = self.parse_number()?;
                 Ok(NumberValue(res))
             }
-            Some('0' ... '9') => {
+            Some('0' ..= '9') => {
                 check_nesting!(self);
                 self.decoded += 1;
                 let res = self.parse_bytestring()?;
@@ -297,7 +297,7 @@ mod test {
     fn parse_error_on_invalid_first_character() {
         for n in ::std::u8::MIN..::std::u8::MAX {
             match n as char {
-                'i' | '0' ... '9' | 'l' | 'd' | 'e' => continue,
+                'i' | '0' ..= '9' | 'l' | 'd' | 'e' => continue,
                 _ => {}
             };
             let msg = format!("Expecting 'i or 0-9 or l or d or e' but got '{}'", alphanum_to_str(n as char));
